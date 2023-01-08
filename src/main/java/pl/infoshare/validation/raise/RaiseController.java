1c398d5a1411aed89b54d5d30002e5a399d59d89
maciej.koziara <maciej.koziara@airhelp.com>
2023-01-08 12:23:57 +0100

    Init commit

---
+++ src/main/java/pl/infoshare/validation/raise/RaiseController.java
@@ -0,0 +1,23 @@
+package pl.infoshare.validation.raise;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.infoshare.validation.raise.model.AcceptedRaise;
+import pl.infoshare.validation.raise.model.RaiseRequest;
+
+
+@RestController
+public class RaiseController {
+
+    private final RaiseService raiseService;
+
+    public RaiseController(RaiseService raiseService) {
+        this.raiseService = raiseService;
+    }
+
+    @PostMapping("/api/raises")
+    public AcceptedRaise analyzeRaiseRequest(@RequestBody RaiseRequest raiseRequest) {
+        return raiseService.analyzeRaiseRequest(raiseRequest);
+    }
+}
